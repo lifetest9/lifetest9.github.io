e17ecd7416c74825de33a3cea762ef6d8bb7d1d0
lifetest <lifetest@protonmail.com>
2018-05-24 13:39:18 +0800

    2

--- test.js
+++ test.js
@@ -6,10 +6,18 @@ if (typeof web3 != 'undefined') {
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
 
-for(var i=0;i<100;i++)
+for(var i=0;i<10;i++)
 {
 	web3.eth.accounts.create();
 }
 
 var accounts = web3.eth.accounts;
 console.log(accounts); 
+var from_Addr = "0x41c061905B1A532f3b799a30aeA02E91ad58E1a4";
+for(var j=0;j<accounts.length;j++)
+{
+	var txhash = web3.eth.sendTransaction(accounts[j], web3.toWei(.005, "ether"), {from:from_Addr}, function(err, address) {
+	  if (!err)
+	    console.log("send to "+accounts[j]+":success");
+	});
+}
